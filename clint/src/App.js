4828d6d6f89165b87eb1105a77134cf5adacd216
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-13 20:42:21 +0600

    input material icon

--- clint/src/App.js
+++ clint/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Home from "./page/home/Home";
+import HomeIcon from "@mui/icons-material";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <HomeIcon />
+    </div>
+  );
 }
 
 export default App;
